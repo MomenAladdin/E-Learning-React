0856e98ffabb7bf846dde628e9673964249859ab
MomenAladdin <moamenaladdin3@gmail.com>
2024-08-26 10:57:38 +0300

    Update apiLinkSlice.js

--- src/redux/reducers/apiLinkSlice.js
+++ src/redux/reducers/apiLinkSlice.js
@@ -3,9 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 let apiLinkSlice = createSlice({
     name: 'apiLink',
     initialState: {
-        link: 'https://e-learning-api-amber.vercel.app'
+        // link: 'https://e-learning-api-amber.vercel.app'
         // link:'http://localhost:5000'
-        // link:'https://e-learning-data-ggx8xbe6j-ahm6daliis-projects.vercel.app'
+        link:'https://e-learning-api-react.vercel.app'
     },
     reducers: {
     }
